1d51342cf96394df91d85a77b9c94a9949434c46
jackzrz <zhourz@etripinfo.net>
2021-05-26 12:30:34 +0800

    init

---
+++ App.vue
@@ -0,0 +1,33 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+			//检测更新
+			//网络监听
+			//初始化数据
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+	@import "./common/uni.css";
+	
+	/*自定义图标库*/
+	@import "./common/icon.css";
+	
+	/*字体动画样式库*/
+	@import "./common/animate.min.css";
+
+	/* 自定义样式库 */
+	@import "./common/free.css";
+	
+	/* 全局样式 */
+	@import "./common/common.css";
+</style>
